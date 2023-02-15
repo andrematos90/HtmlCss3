360c2f818229f341f9e306fe99650d084ab7c224
andre matos <andrermatos90@gmail.com>
2023-02-15 13:33:17 -0300

    Semantica

---
+++ Módulo 5 - Responsividade/13 - Função min e max/estilo.cs
@@ -0,0 +1,11 @@
+img {
+    width: max(50%, 300px);  
+    /* usando a função max
+    valores de tipos diferentes são passados separados por virgula
+     e  o css calcula  qual é o maior valor e aplica
+     50% vai variar e no exemplo se a ela tiver 1000 de largura
+     50% é 500 então é o que sera aplicado no width
+     o mesmo funciona para a função min mas o menor valor sera aplicado no width
+
+    */
+}
